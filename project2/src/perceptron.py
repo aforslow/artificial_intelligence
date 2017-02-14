0ddc0ed2a611a37586cd9732189cbeb70de762f0
anden37 <andreas.forslow@gmail.com>
2017-02-14 20:27:53 +0100

    fixed something

---
+++ project2/src/perceptron.py
@@ -0,0 +1,9 @@
+#usr/bin/env python
+
+class Perceptron():
+
+	def __init__(self):
+		pass
+
+	def update(self):
+		pass
---
+++ project2/src/reader_function.py
@@ -0,0 +1,15 @@
+#usr/bin/env python
+import sys
+sys.path.append('/Users/andreas/Downloads/libsvm-3.22/python')
+from svmutil import *
+
+class Reader():
+
+	def __init__(self):
+		pass
+
+	def read_file(self, filename):
+		pass
+
+	def get_file(self):
+		pass

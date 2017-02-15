7a274bc575210da3dd334a55a9c929943ecffcac
anden37 <andreas.forslow@gmail.com>
2017-02-15 12:47:06 +0100

    Fixed reader function (now creates LIBSVM format of data files)

--- project2/src/reader_function.py
+++ project2/src/reader_function.py
@@ -9,7 +9,30 @@ class Reader():
 		pass
 
 	def read_file(self, filename):
-		pass
+		self.y, self.x = svm_read_problem(filename)
+		return self.y, self.x
 
-	def get_file(self):
-		pass
+	def encode_file(self, source, dest, idx):
+		src_file = open(source, 'r')
+		dest_file = open(dest, 'a')
+		for src_line in src_file:
+			dest_line = ""
+			dest_line += idx
+			ctr = 1
+			for value in src_line.split():
+				dest_line += " " + str(ctr) + ":" + str(value)
+				ctr += 1
+			dest_file.write(dest_line + "\n")
+		src_file.close()
+		dest_file.close()
+
+
+if __name__ == "__main__":
+	r = Reader()
+	#y, x = r.read_file("test_format.txt")
+	with open("../test/test1.txt", "w") as file:
+		file.write("")
+	r.encode_file("../test/salammbo_a_fr.plot.txt", "../test/test1.txt", "1")
+	r.encode_file("../test/salammbo_a_en.plot.txt", "../test/test1.txt", "2")
+	y, x = r.read_file("../test/test1.txt")
+	print y, x

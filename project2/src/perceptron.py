4889e43ea09eec17a595e1e8ddad5025724d2e44
anden37 <andreas.forslow@gmail.com>
2017-02-16 18:18:01 +0100

    Fixed perceptron and reader function

--- project2/src/perceptron.py
+++ project2/src/perceptron.py
@@ -1,9 +1,71 @@
 #usr/bin/env python
+from reader_function import Reader
 
 class Perceptron():
 
-	def __init__(self):
-		pass
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+		self.w = [0] * 3 #We assume 2 features (as in training set)
 
-	def update(self):
-		pass
+	def train(self):
+		print "Training..."
+		print ""
+		miss_rate = 1.0
+		while (miss_rate > 0.1):
+			idx = 0
+			n_misses = 0.0
+			for dictionary in self.x:
+				n_misses += self.simple_update(dictionary, self.y[idx])
+				idx += 1
+			miss_rate = n_misses / len(y)
+
+		print "Miss rate", miss_rate, n_misses, len(y)
+
+	def get_params(self):
+		return self.w
+
+	def classify(self, x):
+		classification_val = self.w[0] + self.w[1]*x[1] + self.w[2]*x[2]
+		guessed_y = 0
+		if classification_val > 0:
+			guessed_y = 1
+		return guessed_y
+
+	def simple_update(self, x, y):
+		guessed_y = self.classify(x)
+		self.w[0] = self.w[0] + (y - guessed_y)
+		self.w[1] = self.w[1] + x[1]*(y - guessed_y)
+		self.w[2] = self.w[2] + x[2]*(y - guessed_y)
+
+		if (y-guessed_y) == 0:
+			return 0
+		return 1
+
+	def simple_test(self):
+		self.test(self.y, self.x)
+
+	def test(self, y, dicts):
+		idx = 0
+		classification_list = []
+		for dictionary in dicts:
+			classification_list.append(self.classify(dictionary))
+		
+		print "Classifications: "
+		print classification_list,
+		print "Actual classes: "
+		print y
+
+if __name__ == "__main__":
+	r = Reader()
+	r.libsvm_read_file("../test/test1.txt")
+	y, x = r.get_randomized_data()
+	p = Perceptron(x, y)
+	p.train()
+	print "params: "
+	print p.get_params()
+	p.simple_test()
+
+
+
+
--- project2/src/reader_function.py
+++ project2/src/reader_function.py
@@ -2,13 +2,14 @@
 import sys
 sys.path.append('/Users/andreas/Downloads/libsvm-3.22/python')
 from svmutil import *
+import random
 
 class Reader():
 
 	def __init__(self):
 		pass
 
-	def read_file(self, filename):
+	def libsvm_read_file(self, filename):
 		self.y, self.x = svm_read_problem(filename)
 		return self.y, self.x
 
@@ -26,13 +27,49 @@ class Reader():
 		src_file.close()
 		dest_file.close()
 
+	def scale_data(self):
+		#Get max & min vals
+		max_vals = []
+		min_vals = []
+		for list_obj in self.x:
+			max_vals.append(max(list_obj.values()))
+			min_vals.append(min(list_obj.values()))
+		min_val = min(min_vals)
+		max_val = max(max_vals)
+
+		#Scale the data
+		for list_obj in self.x:
+			for key in list_obj.keys():
+				list_obj[key] = (list_obj[key] - min_val) / (max_val - min_val)
+
+		return self.x
+
+	def get_randomized_data(self):
+		y, x = self.shuffle(self.y, self.x)
+		return y, x
+
+	def shuffle(self, y, x):
+		indices = [n for n in range(len(self.y))]
+		random.shuffle(indices)
+		rand_y = []
+		rand_x = []
+		for idx in indices:
+			rand_y.append(y[idx])
+			rand_x.append(x[idx])
+		return rand_y, rand_x
 
 if __name__ == "__main__":
 	r = Reader()
 	#y, x = r.read_file("test_format.txt")
 	with open("../test/test1.txt", "w") as file:
 		file.write("")
-	r.encode_file("../test/salammbo_a_fr.plot.txt", "../test/test1.txt", "1")
-	r.encode_file("../test/salammbo_a_en.plot.txt", "../test/test1.txt", "2")
-	y, x = r.read_file("../test/test1.txt")
-	print y, x
+	r.encode_file("../test/salammbo_a_fr.plot.txt", "../test/test1.txt", "0")
+	r.encode_file("../test/salammbo_a_en.plot.txt", "../test/test1.txt", "1")
+	y, x = r.libsvm_read_file("../test/test1.txt")
+	print x
+	print ""
+	z = r.scale_data()
+	z1 = z[0]
+	z2 = z1[1]
+	print z
+	print z2

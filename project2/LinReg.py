3e8025aacbb62e43bc88f752d73030cee19e1862
supernovan <matte94@gmail.com>
2017-02-17 22:49:35 +0100

    Done

--- project2/LinReg.py
+++ project2/LinReg.py
@@ -15,23 +15,23 @@ def partialDerivateKBatch(k, m, x, y):
 	gradient = 0
 	for i in range(0, len(y)):
 		gradient += -x[i]*(y[i] - (k*x[i] + m))
-	return gradient/len(y)
+	return 2*gradient/len(y)
 
 #m as in the m in y = kx + m, batch version
 def partialDerivateMBatch(k, m, x, y):
 	gradient = 0
 	for i in range(0, len(y)):
 		gradient += -(y[i] - (k*x[i] + m))
-	return gradient/len(y)
+	return 2*gradient/len(y)
 #m as in the m in y = kx + m, stoch version
 def partialDerivateKStoch(k, m, x, y):
 	gradient = -x*(y - (k*x + m))
-	return gradient
+	return 2*gradient
 
 #m as in the m in y = kx + m, stoch version
 def partialDerivateMStoch(k, m, x, y):
 	gradient = -(y - (k*x + m))
-	return gradient
+	return 2*gradient
 
 
 
@@ -74,16 +74,17 @@ def createRandomValues(minX, maxX, minY, maxY, nbrOfValues):
 
 
 #testingData (the french version, english is commented out)
-epsilon = 0.0001
+epsilon = 0.001
 epoch = 2000
 m = -1
 k = 0
 alpha = 0.1
+
 yRawE = [36961,43621,15694,36231,29945,40588,75255,37709,30899,25486,37497,40398,74105,76725,18317]
 xRawE = [2503,2992,1042,2487,2014,2805,5062,2643,2126,1784,2641,2766,5047,5312,1215]
 
-yRawE = [2217,2761,990,2274,1865,2606,4805,2396,1993,1627,2375,2560,4597,4871,1119]
-xRawE = [35680,42514,15162,35298,29800,40255,74532,37464,31030,24843,36172,39552,72545,75352,18031]
+#xRawE = [2217,2761,990,2274,1865,2606,4805,2396,1993,1627,2375,2560,4597,4871,1119]
+#yRawE = [35680,42514,15162,35298,29800,40255,74532,37464,31030,24843,36172,39552,72545,75352,18031]
 
 
 x = [float(i)/max(xRawE) for i in xRawE]
@@ -112,7 +113,8 @@ plt.plot([min(x), max(x)], [vec[0]*min(x) + vec[1], vec[0]*max(x) + vec[1]], 'r-
 
 #plotting the batch regression
 plt.plot([min(x), max(x)], [k1*min(x) + m1, k1*max(x) + m1], 'b--')
-
+print("Batch: " + str(k1) + " - k " + str(m1) + " - m")
 #plotting the stoch regression
 plt.plot([min(x), max(x)], [k2*min(x) + m2, k2*max(x) + m2], 'g--')
+print("Stoch: " + str(k1) + " - k " + str(m1) + " - m")
 plt.show()
